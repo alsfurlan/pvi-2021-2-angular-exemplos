502e0ec6cefe6abd2d1d6c3690922d116846462b
Anderson Furlan <alsfurlan@gmail.com>
2021-09-09 22:01:33 -0300

    Criado componente de contador

---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
+})
+export class ContadorComponent implements OnInit {
+
+  // contador = 0;
+  contador: number;
+
+  constructor() {
+    this.contador = 0;
+  }
+
+  ngOnInit(): void {
+  }
+
+  incrementar() {
+    this.contador++;
+  }
+
+  decrementar() {
+    this.contador--;
+  }
+
+}
